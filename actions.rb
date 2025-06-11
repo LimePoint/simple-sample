9efe68173086c81d0b23ada48f4a0bce537ff39a
Stephen Anderson <sanderson@limepoint.com>
2025-06-11 11:55:39 +1000

    Update actions.rb to use the new exec_command method

--- actions.rb
+++ actions.rb
@@ -11,7 +11,8 @@ end
 action :ant_phase, steps: %i[ant_hello ant_welcome], run_as: :parallel
 
 action :shell_hello do
-  sh 'bash ./hello_world.sh'
+  result =exec_command 'bash ./hello_world.sh'
+  OpsChain.logger.info "Failed with: #{result.stderr}" if result.failed?
 end
 
 action :default, steps: %i[ant_phase shell_hello]
