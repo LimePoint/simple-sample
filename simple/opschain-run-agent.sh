953c1b5c7f97219cec3ce1ec7d00363b7f61913b
Bryce Gibson <bgibson@limepoint.com>
2026-02-16 15:14:04 +1100

    WIP.

---
+++ simple/opschain-run-agent.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eo pipefail -o nounset
+
+: "${NODE_ID}"
+: "${AGENT_SCRIPT_PATH}"
+exec </dev/null stdbuf -oL -eL "${AGENT_SCRIPT_PATH}" 2>&1 | stdbuf -i0 /opt/fluent-bit/bin/fluent-bit --config=/opt/fluent-bit/config/agent-fluent-bit.conf
